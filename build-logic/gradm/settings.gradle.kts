644b28427feac3853ce5e7f2252da60085e63f6d
Omico <me@omico.icu>
2023-07-27 04:18:20 -0700

    Update dependencies

--- build-logic/gradm/settings.gradle.kts
+++ build-logic/gradm/settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "elucidator-build-logic-gradm"
+
+pluginManagement {
+    repositories {
+        maven(url = "https://maven.omico.me")
+        gradlePluginPortal()
+    }
+}
--- build-logic/initialization/src/main/kotlin/elucidator.settings.gradle.kts
+++ build-logic/initialization/src/main/kotlin/elucidator.settings.gradle.kts
@@ -1,10 +1,6 @@
-pluginManagement {
-    repositories {
-        maven(url = "https://maven.omico.me")
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
+import me.omico.gradm.addDeclaredRepositories
+
+addDeclaredRepositories()
 
 plugins {
     id("gradm")
